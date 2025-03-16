f1fd4417837e948a01c777c163727fd1979c1f14
khushalsinhmar <shimarsabb@gmail.com>
2025-03-16 15:39:05 +0530

    Create a first route. (According to industry standerd practices)

--- src/app.js
+++ src/app.js
@@ -2,21 +2,28 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+
 const app = express();
+
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
 
+
 app.use(express.json({limit: "100kb"}));
 app.use(express.urlencoded({extended: false, limit: "16kb"}));
 app.use(express.static("public"));
-
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send("Flute")
-})
+
+// Routes Import
+import userRouter from "./routes/user.routes.js";
+
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter)
+
 
 export { app }
